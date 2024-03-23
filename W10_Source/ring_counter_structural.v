54f4cbc77fec9982676a9ab8593f7d8dc4ba9c2d
hun01222 <hun0122232@gmail.com>
2024-03-23 20:25:51 +0900

    new

--- W10_Source/ring_counter_structural.v
+++ W10_Source/ring_counter_structural.v
@@ -6,9 +6,9 @@ module ring_counter_structural_module (clk, rst, preset, out);
 
   output [3:0] out;
 
-  d_flip_flop_behavioral_module d_flip_flop_behavioral0(.d(out[0]), .clk(clk), .rst(rst), .preset(preset[0]), .q(!(out[3])));
-  d_flip_flop_behavioral_module d_flip_flop_behavioral1(.d(out[1]), .clk(clk), .rst(rst), .preset(preset[1]), .q(out[0]));
-  d_flip_flop_behavioral_module d_flip_flop_behavioral2(.d(out[2]), .clk(clk), .rst(rst), .preset(preset[2]), .q(out[1]));
-  d_flip_flop_behavioral_module d_flip_flop_behavioral3(.d(out[3]), .clk(clk), .rst(rst), .preset(preset[3]), .q(out[2]));
+  d_flip_flop_behavioral_module d_flip_flop_behavioral0(.d(!(out[3])), .clk(clk), .rst(rst), .preset(preset[0]), .q(out[0]));
+  d_flip_flop_behavioral_module d_flip_flop_behavioral1(.d(out[0]), .clk(clk), .rst(rst), .preset(preset[1]), .q(out[1]));
+  d_flip_flop_behavioral_module d_flip_flop_behavioral2(.d(out[1]), .clk(clk), .rst(rst), .preset(preset[2]), .q(out[2]));
+  d_flip_flop_behavioral_module d_flip_flop_behavioral3(.d(out[2]), .clk(clk), .rst(rst), .preset(preset[3]), .q(out[3]));
 
 endmodule
