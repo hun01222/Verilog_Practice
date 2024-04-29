1b4a6442029432fe4cc6db11ce7b0cad0e6f6f43
hun01222 <hun0122232@gmail.com>
2024-04-29 20:10:53 +0900

    sw edit

--- RISC-V_singleCycle_Distribution/mainController.v
+++ RISC-V_singleCycle_Distribution/mainController.v
@@ -11,7 +11,7 @@ module mainController
         case(opCode) // ALU Control 파일 가서 확인할 것
             7'b0110011 : control = 10'b0010000010; // R-type // congatination
             7'b0000011 : control = 10'b1111000000; // lw (I-type)
-            7'b0000111 : control = 10'b1000100000; // sw
+            7'b0000111 : control = 10'b0000100000; // sw
             7'b1100011 : control = 10'b0x00010001; // branch (sb-type)    
             7'b0000010 : control = 10'b1010000011; // addi3
             default    : control = 10'bxxxxxxxxxx; // case x 라는걸 써야된다고 함
