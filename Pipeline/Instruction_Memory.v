e555d639f5bfd52c6dccf3e9b4739b4076a81dd5
hun01222 <hun0122232@gmail.com>
2024-06-26 00:33:50 +0900

    final

--- Pipeline/Instruction_Memory.v
+++ Pipeline/Instruction_Memory.v
@@ -34,49 +34,7 @@ module Instruction_Memory (
         // end_inner
         instructions_Value[76] = 32'h00108093; // 000000000001 00001 000 00001 0010011 // addi x1, x1, 1
         instructions_Value[80] = 32'hFC5FF06F; // 1 1111100010 1 11111111 00000 1101111 // jal  x0, outer_loop(20) -> -60 => 11111111 11111111 11111111 11000100
-        
-        /*
-            3'b000:  ImmExtD = {{20{Imm[24]}}, Imm[24:13]};                            // I type
-            3'b001:  ImmExtD = {{20{Imm[24]}}, Imm[24:18], Imm[4:0]};                  // S type (stores)
-            3'b010:  ImmExtD = {{20{Imm[24]}}, Imm[0],  Imm[23:18], Imm[4:1], 1'b0};   // B type (branches)
-            3'b011:  ImmExtD = {{12{Imm[24]}}, Imm[12:5],  Imm[13], Imm[23:14], 1'b0}; // J type (branches)
-        */
-        
-        //The Test Case of mine
-        /*
-        instructions_Value[ 0] = 32'h000181B3; // 000000000000 00011 000 00011 0110011 // addi x3, x3, 0
-        instructions_Value[ 4] = 32'h0011CA63; // 0 000000 00001 00011 100 1010 0 1100011 // blt  x1, x3, 24 -> 20
-	    instructions_Value[ 8] = 32'h00108093; // 000000000001 00001 000 00001 0110011 // addi x1, x1, 1
-        instructions_Value[12] = 32'h00110113; // 000000000001 00010 000 00010 0110011 // addi x2, x2, 1
-        instructions_Value[16] = 32'h00110113; // 000000000001 00010 000 00010 0110011 // addi x2, x2, 1
-        instructions_Value[20] = 32'h00110113; // 000000000001 00010 000 00010 0110011 // addi x2, x2, 1
-        instructions_Value[24] = 32'h00110113; // 000000000001 00010 000 00010 0110011 // addi x2, x2, 1
-        instructions_Value[28] = 32'h00110113; // 000000000001 00010 000 00010 0110011 // addi x2, x2, 1
-        instructions_Value[32] = 32'hFE5FF06F; // 1 1111110010 1 11111111 00000 1101111 // jal  x0, 4 -> -28 => 11111111 11111111 11111111 11100100
-        instructions_Value[36] = 32'h00110113; // 000000000001 00010 000 00010 0110011 // addi x2, x2, 1
-        instructions_Value[40] = 32'h00110113; // 000000000001 00010 000 00010 0110011 // addi x2, x2, 1
-        
 
-        // Given Test Case
-        /*
-        instructions_Value[ 4] = 32'h002081B3; // add  x3, x1, x2 // 
-        instructions_Value[ 8] = 32'h403202B3; // sub  x5, x4, x3        
-	    instructions_Value[12] = 32'h00308383; // lw   x7, 3(x1) // 00000000001100001000001110000011
-        instructions_Value[16] = 32'h0013F333; // and  x6, x7, x1
-        instructions_Value[20] = 32'h001112B3; // sll  x5, x2, x1
-        instructions_Value[24] = 32'h001122B3; // slt  x5, x2, x1
-        instructions_Value[28] = 32'h00210463; // 0 000000 00010 00010 000 0100 0 1100011 // beq  x2, x2, 4 -> -24
-        instructions_Value[32] = 32'h001132B3; // sltu x5, x2, x1  // skipped by beq
-        instructions_Value[36] = 32'h00322123; // sw x3 2(x4)
-        instructions_Value[40] = 32'h001152B3; // srl  x5, x2, x1
-        instructions_Value[44] = 32'h401152B3; // sra  x5, x2, x1
-        instructions_Value[48] = 32'h008002EF; // 0 0000000100 0 00000000 00101 1101111 // jal  x5, 2       // will jump on
-	    instructions_Value[52] = 32'h00110293; // addi x5, x2, 1
-        instructions_Value[56] = 32'h00312293; // slti x5, x2, 3
-        instructions_Value[60] = 32'h00517293; // andi x5, x2, 5
-        instructions_Value[64] = 32'h00211293; // slli x5, x2, 2
-        instructions_Value[68] = 32'h002102E7; // 0 0000000001 0 00010000 00101 1100111 // jar  x5, x2, 2   // will jump on instruction 4
-        */
     end
 
     always @ (*) begin
