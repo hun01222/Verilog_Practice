4a5c58e2c655dd0be65222ee3959068493085924
hun01222 <hun0122232@gmail.com>
2024-06-25 10:48:52 +0900

    new

--- Pipeline/Branch_Predictor.v
+++ Pipeline/Branch_Predictor.v
@@ -21,7 +21,7 @@ module Branch_Predictor (
   initial begin
     
     for(i = 0; i < 255; i = i + 1) begin
-      branch_predictor_PC[i] = 2'b00;
+      branch_predictor_state[i] = 2'b00;
     end
 
     branch_predictor_PC[4] = 32'h00000014;
